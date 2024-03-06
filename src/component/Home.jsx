d8e9df099f16c10ffb4288ce2e584c089b1386e8
Sanjay peringayil <sanjayperingayil@gmail.com>
2024-03-07 02:13:08 +0530

    first page

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import Header from './Header'
+
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
+    <>
+      <Header />
+      <div>
+        <div className="lftBox">
+          <div>
+            <p>Choose your interaction mode : </p><br/>
+            <label>
+              <input type="radio" name="options" value="option1" /> Option 1
+            </label>
+            <br />
+            <label>
+              <input type="radio" name="options" value="option2" /> Option 2
+            </label>
+            <br />
+            <label>
+              <input type="radio" name="options" value="option3" /> Option 3
+            </label>
+          </div>
+        </div>
+        <div className="rghtBox"></div>
+      </div>
+    </>
+  );
 }
 
 export default Home
