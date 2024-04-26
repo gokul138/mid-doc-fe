283fd1c2b3550d5a9940a5965a756a605e5457f0
TinuD <krtinu.d@stplc.info>
2024-04-26 16:20:49 +0530

    CORS error

--- src/component/axiosInstance.js
+++ src/component/axiosInstance.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: 'https://docgeniee.org/mid-doc/',
-  withCredentials: true, // Allow sending cookies with cross-origin requests
 });
 
 
