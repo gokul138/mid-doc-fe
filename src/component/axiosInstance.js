c2f2a84681c3691af7e2797982d822916b1fce6d
TinuD <krtinu.d@stplc.info>
2024-04-26 16:38:20 +0530

    git ignore

--- src/component/axiosInstance.js
+++ src/component/axiosInstance.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://docgeniee.org/mid-doc/',
+  baseURL: process.env.REACT_APP_BASE_URL,
 });
-
 
 export default axiosInstance;
